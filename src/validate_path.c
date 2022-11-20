f2fdc5d164ef465b9d63564f084124ef11d922b9
lguedes <lguedes@student.42.rio>
2022-11-20 10:45:37 -0300

    Valid path working, but movement and exit not.

--- src/validate_path.c
+++ src/validate_path.c
@@ -1,25 +1,6 @@
 #include "so_long.h"
 
-void	rechable_elements(t_data *data, int x, int y, int e_count);
-
-void	validate_path(t_data *data)
-{
-	int e_cnt;
-	int c_cnt;
-
-	// Just for better reading
-	e_cnt = 1;
-	// Keep tracks of how much coins there's in the map
-	c_cnt = data->map.coin_count; // temp for after checking
-	rechable_elements(data, data->player.x, data->player.y, e_cnt);
-	if (data->map.coin_count > 0|| e_cnt > 0)
-		throw_quit(data, ERR_V_P, 1);
-	data->map.coin_count = c_cnt;
-	free(data->map.grid_cpy);
-	return ;
-}
-
-void	rechable_elements(t_data *data, int x, int y, int e_count)
+void	rechable_elements(t_data *data, int x, int y, int *e_count)
 /*
  * This should evaluate each position that a player can move (0's)
  * to find a valid path into all C's and at least one E
@@ -30,10 +11,25 @@ void	rechable_elements(t_data *data, int x, int y, int e_count)
 	if (data->map.grid_cpy[x][y] == 'C')
 		data->map.coin_count--;
 	if (data->map.grid_cpy[x][y] == 'E')
-		e_count--;
+		--*e_count;
 	data->map.grid_cpy[x][y] = '1'; // Changes already known place to a wall
 	rechable_elements(data, (x + 1), y, e_count);
 	rechable_elements(data, (x - 1), y, e_count);
 	rechable_elements(data, x, (y + 1), e_count);
 	rechable_elements(data, x, (y - 1), e_count);
+}
+
+void	validate_path(t_data *data)
+{
+	int e_cnt;
+	int c_cnt;
+
+	e_cnt = 1;
+	c_cnt = data->map.coin_count;
+	rechable_elements(data, data->player.x, data->player.y, &e_cnt);
+	if (data->map.coin_count > 0 || e_cnt > 0)
+		throw_quit(data, ERR_V_P, 1);
+	data->map.coin_count = c_cnt - 1;
+	free(data->map.grid_cpy);
+	return ;
 }
