5fb2b1a832e7217ee7c1d2781b126711fac143e8
lguedes <lguedes@student.42.rio>
2022-11-23 16:00:32 -0300

    Adding hook to close window.

--- src/so_long.c
+++ src/so_long.c
@@ -1,6 +1,13 @@
 #include "so_long.h"
 #include "libft/src/libft.h"
 
+int	x_quit_game(t_data *data)
+{
+	if(data)
+		throw_quit(data, "", 0);
+	return (0);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_data	data;
@@ -14,6 +21,7 @@ int	main(int argc, char *argv[])
 	gen_player(&data, P_ASSET);
 	gen_window(&data, (64 * data.map.width), (64 * data.map.height), "so_long");
 	print_grid(&data);
+	mlx_hook(data.window.ptr, 17, 0, x_quit_game, &data);
 	mlx_key_hook(data.window.ptr, movement_parser, &data);
 	mlx_loop(data.mlx);
 	return (0);
